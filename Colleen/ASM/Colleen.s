782a104f674535af35359ee613ae3e17c246d866
Sunghun Yoon <gnsals6004@gmail.com>
2024-07-07 19:49:36 +0900

    fix: end of string null

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -34,4 +34,4 @@ xor rax, rax
 add rsp, 8
 ret
 section .data
-s db "section .text%cextern _printf%cglobal _main%c;This program will print its own source when run.%c_colleen:%clea rdi, [rel s]%cmov rsi, 0xa%cmov rdx, 0xa%cmov rcx, 0xa%cmov r8, 0xa%cmov r9, 0xa%cpush qword 0xa%cpush qword 0x22%clea rax, [rel s]%cpush rax%cpush qword 0x22%cxor rax, rax%c.loop:%ccmp rax, 0x1F%cjge .done%cpush qword 0xa%cinc rax%cjmp .loop%c.done:%cxor rax, rax%ccall _printf%cadd rsp, 280%cret%c_main:%c;Another function in addition to the main function%csub rsp, 8%ccall _colleen%cxor rax, rax%cadd rsp, 8%cret%csection .data%cs db %c%s%c%c"
+s db "section .text%cextern _printf%cglobal _main%c;This program will print its own source when run.%c_colleen:%clea rdi, [rel s]%cmov rsi, 0xa%cmov rdx, 0xa%cmov rcx, 0xa%cmov r8, 0xa%cmov r9, 0xa%cpush qword 0xa%cpush qword 0x22%clea rax, [rel s]%cpush rax%cpush qword 0x22%cxor rax, rax%c.loop:%ccmp rax, 0x1F%cjge .done%cpush qword 0xa%cinc rax%cjmp .loop%c.done:%cxor rax, rax%ccall _printf%cadd rsp, 280%cret%c_main:%c;Another function in addition to the main function%csub rsp, 8%ccall _colleen%cxor rax, rax%cadd rsp, 8%cret%csection .data%cs db %c%s%c, 0%c", 0
